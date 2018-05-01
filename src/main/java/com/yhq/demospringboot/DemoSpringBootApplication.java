da760388ce23608d38084edaa4bd3189b96e1ddd
yhqnh <yhqnh@aliyun.com>
2018-05-01 23:15:58 +0800

    logback

--- src/main/java/com/yhq/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/com/yhq/demospringboot/DemoSpringBootApplication.java
@@ -1,12 +1,17 @@
 package com.yhq.demospringboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DemoSpringBootApplication {
 
 	public static void main(String[] args) {
+		log.debug("this is debug message================");
+		log.info("this is info message================");
+		log.error("this is error message================");
 		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
 }
